7986dad40a2ac4156f393137814daf33cdab85e9
Artur Shelashskiy <ashelshsky@yandex-team.ru>
2017-09-16 20:35:49 +0500

    resolve child Pipe task fix

--- src/components/Pipe.js
+++ src/components/Pipe.js
@@ -8,11 +8,13 @@ class Pipe extends Task {
             return Promise.reject('No children contains in Pipe task');
         }
 
-        return this.children.reduce((promise:Promise<any>, task:Task) => {
+        return this.children.reduce((promise:Promise<any>, task:Task | Function) => {
             return promise.then(result => {
-                task.setParams(result);
+                const resolvedTask = this.resolveChild(task);
 
-                return task.start();
+                resolvedTask.setParams(result);
+
+                return resolvedTask.start();
             });
         }, Promise.resolve(Object.assign({}, this.params)));
     }
